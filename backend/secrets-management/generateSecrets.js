912917be31609fef8d7e9b1ecc76db16f5bf12e6
Josef Reinhold <josef.r@hotmail.se>
2024-04-03 11:39:41 +0200

    Formated and added comments

--- backend/secrets-management/generateSecrets.js
+++ backend/secrets-management/generateSecrets.js
@@ -1,7 +1,18 @@
+/* 
+   fs module helps you work with files and directories in your computer. It lets you do things like read files,
+   write to files, create new files, delete files, and manage directories. 
+   It's like a toolbox full of tools for handling files and folders in your code.
+*/
 const fs = require('fs');
-const crypto = require('crypto');
-require('dotenv').config();
 
+/*
+   The crypto module helps you work with cryptography, which is all about securing information.
+   It provides tools for creating and verifying digital signatures, encrypting and decrypting data,
+   and generating random numbers. It's like a set of tools for keeping information safe and secure in your code.
+*/
+const crypto = require('crypto');
+
+require('dotenv').config();
 
 // Generate a random secret of specified length (in bytes)
 function generateSecret(length) {
@@ -16,18 +27,21 @@ const REFRESH_TOKEN_SECRET = generateSecret(32); // 256 bits
 const envContent =
 `
 PORT=9999
-MONGO_URI = mongodb+srv://josefr:BytMig123@bitcoininfo.6gdhrq4.mongodb.net/bitcoininfo?retryWrites=true&w=majority&appName=Bitcoininfo
+MONGO_URI=mongodb+srv://josefr:BytMig123@bitcoininfo.6gdhrq4.mongodb.net/bitcoininfo?retryWrites=true&w=majority&appName=Bitcoininfo
 ACCESS_TOKEN_SECRET=${ACCESS_TOKEN_SECRET}
 REFRESH_TOKEN_SECRET=${REFRESH_TOKEN_SECRET}
 `;
 
+// Filepath for where the .env file should be written
 const envFilePath = '../../.env';
 
+// Write content to .env file
 fs.writeFileSync(envFilePath, envContent);
 
+// !!! IMPORTANT !!!
 /*
- run (node generateSecrets.js) in the terminal to generate secrets and write them to the .env file in the root directory of the project
- make sure to be in the correct directory when running the command(secrets-management)
+Run (node generateSecrets.js) in the terminal to generate the secrets and write them to the .env file in the root directory of the project.
+Make sure to be in the correct directory when running the command (secrets-management).
 */
 
 console.log('Secrets generated and written to .env file');
